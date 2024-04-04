1b7ec67b3908c0809908dec3d9532329f40998f3
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-04-04 13:45:10 +0700

    challange 4

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -1,21 +1,6 @@
 const router = require("express").Router();
 
 const Car = require("../controllers/carController");
-const img = require("../models/car");
-const multer = require("multer");
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.filename + "_" + Date.now() + file.originalname);
-  },
-});
-
-var upload = multer({
-  storage: storage,
-}).single("image");
 
 router.get("/", Car.CarPage);
 router.get("/create", Car.createCarPage);
